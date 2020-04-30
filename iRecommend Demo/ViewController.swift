bc896f939881b91c286bedea43b9886cb3cd6d64
Breanna Badalov <BreannaBadalov@gmail.com>
2020-04-30 15:28:25 -0400

    Basics of Project #2.

--- iRecommend Demo/ViewController.swift
+++ iRecommend Demo/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var RecommendationLabel: ViewController!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func SetRandomRecomendation(_ sender: Any) {
+    }
+    
 
 }
 
